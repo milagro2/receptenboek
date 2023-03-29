f8437f8e740e8c5fe13344b49a463d3e1bf0c85a
milagro2 <morenoseijs06@gmail.com>
2023-03-29 11:09:09 +0200

    workingonspecials

---
+++ site/indiaseProjectb3.php
@@ -0,0 +1,75 @@
+<?php require 'header.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/stylingProject.css">
+</head>
+
+<body>
+
+
+    <header>
+        <h1>Indiaas Eten Recepten</h1>
+        <nav>
+            <ul>
+                <li><a href="#Eten">Gerecht</a></li>
+                <li><a href="#lunch">Lunch</a></li>
+                <li><a href="#diner">Diner</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section id="Eten">
+            <h2>Bekende gerechten</h2>
+            <div class="recipe">
+                <img class="ButterChicken" src="C:\school\projectBlok3\receptenboek\site\images\butter-chicken-.jpg">
+                <h3>Butter Chicken</h3>
+                <p>Butter Chicken is een gerecht met kip wat gemarineerd is in een licht gekruide tomatensaus.
+                    Het wordt vaak geserveerd met rijst en/of naan</p>
+            </div>
+            <div class="recipe">
+                <img src="">
+                <h3>Dosa</h3>
+                <p>Dosa is een knapperige pannenkoek gemaakt van rijst en zwarte linzen. Het is een populaire
+                    ontbijt gerecht in Zuid India.</p>
+            </div>
+        </section>
+
+        <section id="lunch">
+            <h2>Lunch Gerechten</h2>
+            <div class="recipe">
+                <img src="">
+                <h3>Chana Masala</h3>
+                <p>Chana masala is </p>
+            </div>
+
+            <div class="recipe">
+                <img src="">
+                <h3>Biryani</h3>
+                <p>Biryani is </p>
+            </div>
+        </section>
+
+        <section id="diner">
+            <h2>Hoofd Gerechten</h2>
+            <div class="recipe">
+                <img src="">
+                <h3>Palak Paneer</h3>
+                <p>Palak paneer is
+
+        </section>
+    </main>
+
+
+
+    <!-- 5 recepten indiase eten -->
+</body>
+
+</html>
